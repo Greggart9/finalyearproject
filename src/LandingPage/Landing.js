fe655b65c728522308b450d730887afa47728da2
Greggart9 <retrievergolden498@gmail.com>
2024-02-19 16:42:45 +0100

    Done with About Us

--- src/LandingPage/Landing.js
+++ src/LandingPage/Landing.js
@@ -63,7 +63,7 @@ const Landing = () => {
              {/* The ring image by the bottom THE IMAGE*/}
 
              <div className='absolute bottom-0 left-[35rem]'>
-                <img className='h-[20rem] -z-1' src='Images/img2.png' alt='The ring' />
+                <img className='h-[20rem] -z-3' src='Images/img2.png' alt='The ring' />
              </div>
         </div>
 
@@ -104,6 +104,81 @@ const Landing = () => {
                 </div>
             </div>
 
+          </section>
+
+
+          {/* THE ABOUT US PAGE */}
+
+          <section className='flex items-center h-[35rem] bg-gray-50'>
+            <div className='container flex mx-auto w-10/12 items-center justify-between'>
+
+                {/* IMAGE OF ABOUT US */}
+                <div className='flex-1'>
+                    <img className='h-96' src='Images/img6.png' alt='about us' />
+                </div>
+
+                {/* ABOUT US TEXT */}
+                <div className='flex-1 space-y-5'>
+                    <p className='font-bold text-3xl'><span className='text-blue-400'>About</span> Us</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat.<br/>
+                        <br/>
+                        
+
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+
+                    <button className='bg-blue-400 px-7 py-2 rounded-3xl text-white font-bold'>Read More</button>
+                </div>
+
+            </div>
+          </section>
+
+          {/* THE AFTER ABOUT US */}
+
+          <section className='bg-yellow-200 flex items-center h-[33rem] '>
+            <div className='container mx-auto flex w-10/12 items-center justify-between'>
+
+                {/* LEFT CONTENT */}
+                <div className='space-y-3'>
+                    <img className='h-[17rem]' src='/Images/img8.png' alt='A lady' />
+                    <p className='text-3xl font-bold'>Explore 1k+ Free <span className='text-blue-400'>Online Resources</span></p>
+                    <p className='text-lg'>Lorem ipsum dolor sit amet, consectetur adipisicing<br/>
+                        elit, sed do eiusmod tempor incididunt ut labore et<br/>
+                        dolore magna aliqua. </p>
+                </div>
+
+                {/* RIGHT CONTENT (THE GRID) */}
+                <div>
+                    <div className='grid grid-cols-2 gap-6'>
+                        <div className='bg-gray-100 pl-5 py-7 px-9 rounded-xl '>
+                            <img className='h-16' src='/Images/img3.png' alt='grid 1' />
+                            <p>Upload Materials</p>
+                        </div>
+
+                        <div className='bg-gray-100 pl-5 py-7 px-9 rounded-xl '>
+                        <img className='h-16' src='/Images/img4.png' alt='grid 2' />
+                        <p>Access Past <br/> Questions</p>
+                        </div>
+
+                        <div className='bg-gray-100 pl-5 py-7 px-9 rounded-xl '>
+                        <img className='h-16' src='/Images/img5.png' alt='grid 3' />
+                        <p>Study Resources</p>
+                        </div>
+
+                        <div className='bg-gray-100 pl-5 py-7 px-9 rounded-xl '>
+                        <img className='h-16' src='/Images/img9.png' alt='grid 4' />
+                        <p>Open Source <br/>Collaboration</p> 
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
           </section>
 
     
