4373066db70f4488c7055afc6b406f87f7dbbf6e
Greggart9 <retrievergolden498@gmail.com>
2024-03-01 13:09:00 +0100

    I just started the sign up page 01/03/24

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Landing from './LandingPage/Landing'
 import Signin from './SignIn/Signin';
+import Signup from './SignUp/Signup';
 
 function App() {
   return (
     <>
       {/* <Landing/> */}
-      <Signin/>
+      {/* <Signin/> */}
+      <Signup/>
     </>
   );
 }
