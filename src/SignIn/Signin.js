1d3362342465fc5f9419c68fef937a4af47ade2a
Greggart9 <retrievergolden498@gmail.com>
2024-03-01 13:03:47 +0100

    Done with sign in page

--- src/SignIn/Signin.js
+++ src/SignIn/Signin.js
@@ -3,31 +3,32 @@ import React from 'react'
 const Signin = () => {
   return (
     <>
-    <section className='flex h-screen mx-auto justify-center items-center'>
-            <div className='container flex  bg-red-200'>
+    <section className='flex h-screen justify-center items-center'>
+      <div className='flex w-full'>
 
         {/* SIGNIN PART */}
-        <div className='flex-1 bg-green-200'>
-            <p className='text-blue-400 font-bold text-2xl'>Sign In</p>
+        <div className='w-3/5 flex flex-col justify-center items-center'>
+          <div>
+            <p className='text-blue-400 font-bold text-3xl'>Sign In</p>
 
-            <p className='text-gray-400 mt-3 text-sm font-bold'>Email</p>
-            <input className='bg-gray-100 h-8 w-[18rem]' type='email' id='email' name='email' />
+                <p className='text-gray-400 mt-3 text-md font-bold'>Email</p>
+                <input className='bg-gray-200 h-10 w-[21rem] rounded outline-gray-400' type='email' id='email' name='email' />
 
-            <p className='text-gray-400 mt-2 text-sm font-bold'>Password</p>
-            <input className='bg-gray-100 h-8 w-[18rem]' type='password' id='password' name='password' />
+                <p className='text-gray-400 mt-2 text-md font-bold'>Password</p>
+                <input className='bg-gray-200 h-10 w-[21rem] rounded outline-gray-400' type='password' id='password' name='password' />
 
-            <p className='text-blue-400 font-bold mt-2'>Forgot Password?</p>
+                <p className='text-blue-400 font-bold mt-2'>Forgot Password?</p>
 
-            <button className='bg-gray-100 py-3 w-[18rem] rounded-3xl mt-3 text-bold'>Sign In</button>
+                <button className='bg-gray-200 py-3 w-[21rem] rounded-3xl mt-3 font-bold text-gray-600'>Sign In</button>
 
-            <p className=' text-sm font-bold'>Don't have an Account? <span className='text-blue-400'>Sign Up</span></p>
-
-        </div>
+                <p className='text-gray-400 text-sm font-bold mt-1'>Don't have an Account? <span className='text-blue-400'>Sign Up</span></p>
+          </div>
+         </div>
 
         {/* IMAGE */}
 
-        <div className='flex-1 bg-gray-200'>
-            <img className='h-96' src='/Images/img11.png' alt='sign in' />
+        <div className='w-2/5 flex justify-end'>
+            <img className='h-screen justify-left w-11/12' src='/Images/img11.png' alt='sign in' />
 
         </div>
 
