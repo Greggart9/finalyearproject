edb9a80cafcf2376965e5fe274f05867e88f4c6a
Greggart9 <retrievergolden498@gmail.com>
2024-03-17 02:19:22 +0100

    dashboard

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Landing from './LandingPage/Landing'
 import Signin from './SignIn/Signin';
 import Signup from './SignUp/Signup';
 import Signup2 from './SignUp2/Signup2';
+import Dboard from './Dashboard/Dboard';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <Landing/> */}
       {/* <Signin/> */}
       {/* <Signup/> */}
-      <Signup2/>
+      {/* <Signup2/> */}
+      <Dboard/>
     </>
   );
 }
---
+++ src/Dashboard/Dboard.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Dboard = () => {
+  return (
+    <>
+    <section className='bg-green-900 h-screen w-64 text-white '>
+
+        <div>
+            <img src='' alt='funaab logo' />
+
+            <div>
+            <img src='' alt='dashboard logo'/> <p>Dashboard</p>    
+            </div>
+
+            <div>
+            <img src='' alt='settings logo'/> <p>Settings</p>    
+            </div>
+        </div>
+
+
+        <div>
+        <img src='' alt='logout logo'/> <p>Logout</p>    
+        </div>
+
+    </section>
+    </>
+  )
+}
+
+export default Dboard
